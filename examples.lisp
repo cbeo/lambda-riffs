7c6a9b5f456235662f95b2f2d0e75058e94af1c6
Colin Okay <cbeok@protonmail.com>
2020-08-13 12:59:09 -0500

    updated example

--- examples.lisp
+++ examples.lisp
@@ -5,10 +5,11 @@
 
 ;; http://rosettacode.org/wiki/Luhn_test_of_credit_card_numbers 
 (defun luhn (n)
-  (flet ((sum-dig (x) (if (> x 9) (- x 9) x)))
-    (>> n #'reverse
-        #$(map 'list #'digit-char-p $char)
-        #$(mapcar #'*
+  (>> n #'reverse
+      #$(map 'list #'digit-char-p $char)
+      #$(mapcar #'*
                 (loop :for i :upto (length $digits) :collect (1+ (mod i 2)))
                 $digits)
-        #$(zerop (mod (apply #'+ (mapcar #'sum-dig $digits)) 10)))))
+      #$(mapcar #$$(if (> $$x 9) (- $$x 9) $$x) $digits)
+      #$(zerop (mod (apply #'+ $digits) 10))))
+
