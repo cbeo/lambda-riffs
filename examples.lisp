61c06fc10ee2bad8d810d342a707677ede2c5f78
Colin Okay <cbeok@protonmail.com>
2020-08-13 09:52:32 -0500

    added example

---
+++ examples.lisp
@@ -0,0 +1,15 @@
+
+(defpackage #:lt-examples
+  (:use #:cl #:lambda-toosl))
+
+
+;; http://rosettacode.org/wiki/Luhn_test_of_credit_card_numbers 
+(defun luhn (n)
+  (flet ((sum-dig (x) (if (> x 9) (- x 9) x)))
+    (>> n
+        #'reverse
+        ($$ (map 'list #'digit-char-p $char))
+        ($$ (mapcar #'*
+                    (loop :for i :upto (length $digits) :collect (1+ (mod i 2)))
+                    $digits))
+        ($$ (zerop (mod (apply #'+ (mapcar #'sum-dig $digits)) 10))))))
